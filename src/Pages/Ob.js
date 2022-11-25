ed8a68b88549d94b711f2e25e247d88d37b19d08
Corrie <corriebotha64@gmail.com>
2022-11-25 11:22:43 +0200

    Add functionality => to add filter when you want to search for anything specific in the ob history

--- src/Pages/Ob.js
+++ src/Pages/Ob.js
@@ -1,27 +1,38 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useMemo } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+
+// Todo key error on click every item in a list should have a unique key
 
 const Ob = () => {
   const [items, setItems] = useState([]);
+  const [query, setQuery] = useState("");
   const inputRef = useRef();
 
-  // Const helper function to set the state to the input field value
+  const filteredItems = useMemo(() => {
+    // ***Reasing for using UseMemo hook => To only compute the values when needed to help with performance and page rerender***
+    return items.filter((item) => {
+      return item.toLowerCase().includes(query);
+    });
+    // ***UseMemo has to return a array with dependencies to update the state
+  }, [items, query]);
+  // **Create helper function to set the state to the input field value
   const handleSumbit = (e) => {
-    // pervent the defualt state of the form
+    // **pervent the defualt state of the form
     e.preventDefault();
-    // Get value
+    // **Get value
     const value = inputRef.current.value;
-    // if value === '' return nothing
+    // **if value === '' return nothing
     if (value === "") {
       return;
     }
-    // Update state
+    // **Update state
     setItems((prev) => {
       // make a copy of the previous item
       // return the copy of the array with the value of the input field
       return [...prev, value];
     });
-    // set the value of the inputRef to a empty string
+
+    // set the value of the inputRef to a empty string (Value from the form)
     inputRef.current.value = "";
   };
 
@@ -33,10 +44,14 @@ const Ob = () => {
           Our Ob Entry system can be used to manage entry and exiting at
           complexes, Shift change of guards etc..
         </h2>
-        <Form onSubmit={handleSumbit} key={items}>
+        <Form onSubmit={handleSumbit}>
           <Form.Group>
             <Form.Label>Search: </Form.Label>
-            <Form.Control type="text" className="mb-3"></Form.Control>
+            <Form.Control
+              type="text"
+              className="mb-3"
+              onChange={(e) => setQuery(e.target.value)}
+            ></Form.Control>
           </Form.Group>
           <Form.Group>
             <Form.Label>Entry: </Form.Label>
@@ -46,16 +61,17 @@ const Ob = () => {
               ref={inputRef}
             ></Form.Control>
           </Form.Group>
-          <Button className="btn-success" type="submit">
+          <Button className="btn-success m-2" type="submit">
             Sumbit
           </Button>
-          <Button className="btn-success" type="submit">
+          <Button className="btn-success m-2" type="submit">
             Remove
           </Button>
-          <h2>
-            Ob:{" "}
-            {items.map((item) => {
-              return <div key={item}>{item}</div>;
+          <h2 key={items}>
+            {/* Returna div with the value of the itme */}
+            Ob:
+            {filteredItems.map((item) => {
+              return <div>{item}</div>;
             })}
           </h2>
         </Form>
